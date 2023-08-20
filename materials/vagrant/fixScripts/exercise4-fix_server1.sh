aace7b810e9c034e3ec532fef585600decc98c65
TrinZola <142475611+TrinZola@users.noreply.github.com>
2023-08-20 12:37:46 +0300

    Update exercise4-fix_server1.sh

--- materials/vagrant/fixScripts/exercise4-fix_server1.sh
+++ materials/vagrant/fixScripts/exercise4-fix_server1.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-#add fix to exercise4-server1 here
+
+# Server information
+server1_name="server1"
+server1_ip="192.168.60.10"
+
+# Add server1 entry to /etc/hosts
+if ! grep -q "$server1_name" /etc/hosts; then
+    echo "$server1_ip $server1_name" | sudo tee -a /etc/hosts > /dev/null
+    echo "Added entry for $server1_name"
+else
+    echo "Entry for $server1_name already exists"
+fi
