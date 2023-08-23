5d9cabe24145712865ba74b42499c6a106efa970
TrinZola <142475611+TrinZola@users.noreply.github.com>
2023-08-23 09:22:46 +0300

    Update exercise5-fix_server2.sh

--- materials/vagrant/fixScripts/exercise5-fix_server2.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server2.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-# Generate SSH key pair if not already done
+# Script to set up passwordless SSH on server2 (192.168.60.11)
+
+# Generate SSH key if it doesn't exist
 if [ ! -f ~/.ssh/id_rsa.pub ]; then
+    echo "SSH key doesn't exist. Generating one..."
     ssh-keygen -t rsa -b 4096 -N "" -f ~/.ssh/id_rsa
 fi
 
 # Copy the public key to server2
-ssh-copy-id -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa.pub vagrant@192.168.60.10
+echo "Copying SSH public key to server2 (192.168.60.11)..."
+ssh-copy-id -i ~/.ssh/id_rsa.pub 192.168.60.11
 
-# Test the SSH connection
-ssh -o StrictHostKeyChecking=no vagrant@192.168.60.10 "echo SSH connection established"
+echo "Passwordless SSH setup for server2 complete!"
