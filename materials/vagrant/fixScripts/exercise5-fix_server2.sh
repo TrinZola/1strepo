63f7839753cc46f9e911431af7504f387ee8214e
TrinZola <142475611+TrinZola@users.noreply.github.com>
2023-08-23 16:04:38 +0300

    Update exercise5-fix_server2.sh

--- materials/vagrant/fixScripts/exercise5-fix_server2.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server2.sh
@@ -20,4 +20,4 @@ echo "UserKnownHostsFile /dev/null" >> /home/$USER/.ssh/config
 chmod 600 /home/$USER/.ssh/config
 
 # SSH into server1 to add its public key
-ssh -o "StrictHostKeyChecking=no" -i /home/$USER/.ssh/id_rsa $USER@$SERVER1 "echo '$PUBLIC_KEY' >> /home/$USER/.ssh/authorized_keys"
+# ssh -o "StrictHostKeyChecking=no" -i /home/$USER/.ssh/id_rsa $USER@$SERVER1 "echo '$PUBLIC_KEY' >> /home/$USER/.ssh/authorized_keys"
