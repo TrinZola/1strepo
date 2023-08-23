2d96ce25f96bfbcd5c83a7096e56a8036291d8ab
TrinZola <142475611+TrinZola@users.noreply.github.com>
2023-08-23 09:16:28 +0300

    Update exercise5-fix_server1.sh

--- materials/vagrant/fixScripts/exercise5-fix_server1.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server1.sh
@@ -7,6 +7,3 @@ fi
 
 # Copy the public key to server2
 ssh-copy-id -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa.pub vagrant@192.168.60.11
-
-# Test the SSH connection
-ssh -o StrictHostKeyChecking=no vagrant@192.168.60.11 "echo SSH connection established"
