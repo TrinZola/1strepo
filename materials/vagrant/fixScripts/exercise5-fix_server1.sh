b1e8314f7f4dd47e74c06e940018e76107ca9d8b
TrinZola <142475611+TrinZola@users.noreply.github.com>
2023-08-23 16:20:44 +0300

    Update exercise5-fix_server1.sh

--- materials/vagrant/fixScripts/exercise5-fix_server1.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server1.sh
@@ -5,7 +5,7 @@ SERVER2="192.168.60.11"
 # User name
 USER="vagrant"
 # Public key to insert
-PUBLIC_KEY="kasfkalfhfieiewfFEWcdsdk543511!!23%5^%"
+PUBLIC_KEY="AAAAB3NzaC1yc2EAAAADAQABAAAAgQCM+JP+eZM7E5j+G5lN1jgIxrzfU7okANXDfOuqNnuO5Sn9DwSjCKpFOFCUicPDrcDyaIw3FXgnoVAQ4G9upchE+r+2IYuaU3CKqeW18b+zbkjgM7SaRzdi0qvp+7tBuJPGV9ZVQ/rm55rFLB7QadQ+soYhABVvfoXhQUYQ3iNhTw== "
 
 # Create SSH directory if not exists
 mkdir -p /home/$USER/.ssh
