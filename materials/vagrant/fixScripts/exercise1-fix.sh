4fcfed82c01ad1369742eed65492da7aa0777362
TrinZola <142475611+TrinZola@users.noreply.github.com>
2023-08-23 10:02:17 +0300

    Update exercise1-fix.sh

--- materials/vagrant/fixScripts/exercise1-fix.sh
+++ materials/vagrant/fixScripts/exercise1-fix.sh
@@ -1,2 +1,26 @@
 #!/bin/bash
-#add fix to exercise1 here
+
+# Check if the script is being run as root
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root."
+    exit 1
+fi
+
+# DNS address to add
+dns_address="8.8.8.8"
+
+# Backup the original resolved.conf file
+cp /etc/systemd/resolved.conf /etc/systemd/resolved.conf.bak
+
+# Check if the address is already present
+if ! grep -q "^DNS=$dns_address" /etc/systemd/resolved.conf; then
+    echo "DNS=$dns_address" >> /etc/systemd/resolved.conf
+    echo "Added DNS address: $dns_address"
+else
+    echo "DNS address $dns_address is already present."
+fi
+
+# Restart systemd-resolved service
+systemctl restart systemd-resolved
+
+echo "DNS address $dns_address added to resolved.conf. systemd-resolved service restarted."
