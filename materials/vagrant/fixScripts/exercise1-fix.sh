546658bdeffaad51e3000fb2b8549ec71bdab0be
TrinZola <142475611+TrinZola@users.noreply.github.com>
2023-08-23 18:21:56 +0300

    Update exercise1-fix.sh

--- materials/vagrant/fixScripts/exercise1-fix.sh
+++ materials/vagrant/fixScripts/exercise1-fix.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
 
-# Define the network interface and destination
-INTERFACE="enp0s8"
-DESTINATION="www.textfiles.c"
-
-# Check if the route entry exists
-EXISTING_ROUTE=$(ip route show | grep "$DESTINATION")
-
-if [ -n "$EXISTING_ROUTE" ]; then
-    echo "Removing route for $DESTINATION from $INTERFACE..."
-    sudo ip route del "$DESTINATION" dev "$INTERFACE"
-    echo "Route removed successfully."
-else
-    echo "Route for $DESTINATION on $INTERFACE not found."
-fi
+sudo ip route del "208.86.224.90"  
