a9625b6fc24e93253f4f715de9633b417ff62ed2
TrinZola <142475611+TrinZola@users.noreply.github.com>
2023-08-23 11:59:38 +0300

    Update exercise5-fix_server2.sh

--- materials/vagrant/fixScripts/exercise5-fix_server2.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server2.sh
@@ -7,10 +7,10 @@ public_key=$(cat /home/vagrant/.ssh/id_rsa.pub)
 mkdir -p /home/vagrant/.ssh
 chmod 700 /home/vagrant/.ssh
 echo "$public_key" >> /home/vagrant/.ssh/authorized_keys
-chmod 600 /home/vagrant/.ssh/authorized_keys
+sudo chmod 600 /home/vagrant/.ssh/authorized_keys
 
 # Configure SSH options
 echo "Host 192.168.*.*" >> /home/vagrant/.ssh/config
 echo "StrictHostKeyChecking no" >> /home/vagrant/.ssh/config
 echo "UserKnownHostsFile /dev/null" >> /home/vagrant/.ssh/config
-chmod 600 /home/vagrant/.ssh/config
+sudo chmod 600 /home/vagrant/.ssh/config
