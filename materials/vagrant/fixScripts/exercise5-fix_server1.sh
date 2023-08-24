647656232a77a6df6b9640f579d29ff0c8a4a312
TrinZola <142475611+TrinZola@users.noreply.github.com>
2023-08-24 11:28:17 +0300

    Update exercise5-fix_server1.sh

--- materials/vagrant/fixScripts/exercise5-fix_server1.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server1.sh
@@ -37,10 +37,11 @@ edit_sshd_config
 echo "Debugging info for Server:"
 ssh "$USERNAME@$SERVER" tail -n 20 /var/log/auth.log
 
-# Revert sshd_config changes for server
-sudo sed -i 's/^SyslogFacility.*/#SyslogFacility AUTH/' /etc/ssh/sshd_config
-sudo sed -i 's/^LogLevel.*/#LogLevel INFO/' /etc/ssh/sshd_config
-sudo sed -i "s|^AuthorizedKeysFile.*|#AuthorizedKeysFile .ssh/authorized_keys $AUTHORIZED_KEYS_FILE_PATH|" /etc/ssh/sshd_config
+# Disable authorization prompt for Server 2
+echo "Host 192.168.60.11" >> /home/$USERNAME/.ssh/config
+echo "    StrictHostKeyChecking no" >> /home/$USERNAME/.ssh/config
+echo "    UserKnownHostsFile /dev/null" >> /home/$USERNAME/.ssh/config
+chmod 600 /home/$USERNAME/.ssh/config
 sudo systemctl restart sshd
 
 echo "Passwordless SSH setup completed for Server!"
