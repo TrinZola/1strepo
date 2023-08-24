e876125ad305a863ccc9cb9bca4070a8a497d7aa
TrinZola <142475611+TrinZola@users.noreply.github.com>
2023-08-24 17:35:51 +0300

    Update exercise5-fix_server2.sh

--- materials/vagrant/fixScripts/exercise5-fix_server2.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server2.sh
@@ -1,15 +1,38 @@
 #!/bin/bash
 
-# Script to set up passwordless SSH on server2 (192.168.60.11)
+# Run the entire script with elevated privileges
+sudo bash <<EOF
 
-# Generate SSH key if it doesn't exist
-if [ ! -f ~/.ssh/id_rsa.pub ]; then
-    echo "SSH key doesn't exist. Generating one..."
-    ssh-keygen -t rsa -b 4096 -N "" -f ~/.ssh/id_rsa
-fi
+# Define the source and destination servers
+SERVER1_IP="192.168.60.10"
+SERVER2_IP="192.168.60.11"
 
-# Copy the public key to server2
-echo "Copying SSH public key to server2 (192.168.60.11)..."
-ssh-copy-id -i ~/.ssh/id_rsa.pub 192.168.60.11
+# Define the source and destination paths
+SOURCE_PATH="/home/vagrant/.ssh/authorized_keys"
+DEST_PATH="~/"
 
-echo "Passwordless SSH setup for server2 complete!"
+# Create .ssh directory with appropriate permissions if it doesn't exist
+echo "Creating .ssh directory and setting permissions..."
+mkdir -p ~/.ssh && sudo chmod 700 ~/.ssh
+
+# Generate an SSH key pair (ECDSA)
+echo "Generating SSH key pair..."
+ssh-keygen -t ecdsa -f ~/.ssh/id_ecdsa -N ""
+
+# Add the private key to the SSH agent
+echo "Adding private key to SSH agent..."
+ssh-add ~/.ssh/id_ecdsa
+
+# Copy the public key to server1 for authentication
+echo "Copying public key to server1 for authentication..."
+ssh-copy-id -i ~/.ssh/id_ecdsa.pub "vagrant@${SERVER1_IP}"
+
+# Debug: Display the public key for troubleshooting
+echo "Contents of public key:"
+cat ~/.ssh/id_ecdsa.pub
+
+# Copy the file from server2 to server1
+echo "Copying file from server2 to server1..."
+scp "${SERVER2_IP}:${SOURCE_PATH}" "${SERVER1_IP}:${DEST_PATH}"
+
+echo "File copied successfully!"
