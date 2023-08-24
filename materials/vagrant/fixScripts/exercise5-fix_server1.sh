5c527b1db28c0dcff1624cd369aba2425ad16c82
TrinZola <142475611+TrinZola@users.noreply.github.com>
2023-08-24 15:32:43 +0300

    Update exercise5-fix_server1.sh

--- materials/vagrant/fixScripts/exercise5-fix_server1.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Run the entire script with elevated privileges
+sudo bash <<EOF
+
 # Define the source and destination servers
 SERVER1_IP="192.168.60.10"
 SERVER2_IP="192.168.60.11"
@@ -10,7 +13,7 @@ DEST_PATH="~/"
 
 # Create .ssh directory with appropriate permissions if it doesn't exist
 echo "Creating .ssh directory and setting permissions..."
-sudo mkdir -p ~/.ssh && sudo chmod 700 ~/.ssh
+mkdir -p ~/.ssh && sudo chmod 700 ~/.ssh
 
 # Generate an SSH key pair (ECDSA)
 echo "Generating SSH key pair..."
@@ -18,11 +21,11 @@ ssh-keygen -t ecdsa -f ~/.ssh/id_ecdsa -N ""
 
 # Add the private key to the SSH agent
 echo "Adding private key to SSH agent..."
-sudo ssh-add ~/.ssh/id_ecdsa
+ssh-add ~/.ssh/id_ecdsa
 
 # Copy the public key to server1 for authentication
 echo "Copying public key to server1 for authentication..."
-sudo ssh-copy-id -i ~/.ssh/id_ecdsa.pub "vagrant@${SERVER1_IP}"
+ssh-copy-id -i ~/.ssh/id_ecdsa.pub "vagrant@${SERVER1_IP}"
 
 # Debug: Display the public key for troubleshooting
 echo "Contents of public key:"
