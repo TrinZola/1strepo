aa89757686812fcf32f3b0c174da0ed348645546
TrinZola <142475611+TrinZola@users.noreply.github.com>
2023-08-24 12:28:29 +0300

    Update exercise5-fix_server2.sh

--- materials/vagrant/fixScripts/exercise5-fix_server2.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server2.sh
@@ -1,49 +1,26 @@
 #!/bin/bash
 
-# Define variables
-SERVER="192.168.60.10"
-USERNAME="vagrant"
-AUTHORIZED_KEYS_FILE_PATH="/home/$USERNAME/.ssh/authorized_keys"
-PUBLIC_KEY=$(cat ~/.ssh/id_rsa.pub)
+# Server details
+SERVER1_IP="192.168.60.10"
+SERVER2_IP="192.168.60.11"
+USER="vagrant"
 
-# Create SSH directory if not exists
-mkdir -p /home/$USERNAME/.ssh
+# Generate SSH keys
+ssh-keygen -t rsa -b 4096 -f ~/.ssh/server1_key -N ""
+ssh-keygen -t rsa -b 4096 -f ~/.ssh/server2_key -N ""
 
-# Set up authorized_keys with the specified public key
-echo "$PUBLIC_KEY" >> /home/$USERNAME/.ssh/authorized_keys
-chmod 600 /home/$USERNAME/.ssh/authorized_keys
-
-# Set SSH configuration to skip host key checking
-echo "StrictHostKeyChecking no" >> /home/$USERNAME/.ssh/config
-echo "UserKnownHostsFile /dev/null" >> /home/$USERNAME/.ssh/config
-chmod 600 /home/$USERNAME/.ssh/config
-
-# Copy public key to remote server
-ssh-copy-id -i $HOME/.ssh/id_rsa.pub "$USERNAME@$SERVER"
-
-# Function to edit sshd_config and restart sshd
-edit_sshd_config() {
-    sudo sed -i 's/^#SyslogFacility.*/SyslogFacility AUTH/' /etc/ssh/sshd_config
-    sudo sed -i 's/^#LogLevel.*/LogLevel INFO/' /etc/ssh/sshd_config
-    sudo sed -i "s|^#AuthorizedKeysFile.*|AuthorizedKeysFile $AUTHORIZED_KEYS_FILE_PATH|" /etc/ssh/sshd_config
-    sudo systemctl restart sshd
-    sleep 5
+# Copy keys to servers
+copy_keys() {
+    sshpass -p "" ssh-copy-id -i ~/.ssh/server1_key.pub $USER@$SERVER1_IP
+    sshpass -p "" ssh-copy-id -i ~/.ssh/server2_key.pub $USER@$SERVER2_IP
 }
 
-# Enable debugging temporarily for server
-edit_sshd_config
+# Disable server authorization
+disable_server_auth() {
+    ssh -i ~/.ssh/server1_key $USER@$SERVER1_IP "echo 'StrictHostKeyChecking no' >> ~/.ssh/config"
+    ssh -i ~/.ssh/server2_key $USER@$SERVER2_IP "echo 'StrictHostKeyChecking no' >> ~/.ssh/config"
+}
 
-# Check auth.log for debugging info on server
-echo "Debugging info for Server:"
-ssh "$USERNAME@$SERVER" tail -n 20 /var/log/auth.log
-
-# Disable authorization prompt for Server 2
-echo "Host 192.168.60.11" >> /home/$USERNAME/.ssh/config
-echo "    StrictHostKeyChecking no" >> /home/$USERNAME/.ssh/config
-echo "    UserKnownHostsFile /dev/null" >> /home/$USERNAME/.ssh/config
-chmod 600 /home/$USERNAME/.ssh/config
-sudo systemctl restart sshd
-
-echo "Passwordless SSH setup completed for Server!"
-
-exit 0
+# Run functions
+copy_keys
+disable_server_auth
