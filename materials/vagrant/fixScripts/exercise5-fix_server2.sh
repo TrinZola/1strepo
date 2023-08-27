719e6bb897b75f309e2bce60c49ec40ee3b79952
TrinZola <142475611+TrinZola@users.noreply.github.com>
2023-08-27 08:28:31 +0300

    Update exercise5-fix_server2.sh

--- materials/vagrant/fixScripts/exercise5-fix_server2.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server2.sh
@@ -13,27 +13,26 @@ DEST_PATH="~/"
 
 # Create .ssh directory with appropriate permissions if it doesn't exist
 echo "Creating .ssh directory and setting permissions..."
-mkdir -p ~/.ssh && sudo chmod 700 ~/.ssh
+mkdir -p /home/vagrant/.ssh && sudo chmod 700 ~/.ssh
 
 # Generate an SSH key pair (ECDSA)
 echo "Generating SSH key pair..."
-ssh-keygen -t ecdsa -f ~/.ssh/id_ecdsa -N ""
+ssh-keygen -t ecdsa -f /home/vagrantssh/id_ecdsa -N ""
 
 # Add the private key to the SSH agent
 echo "Adding private key to SSH agent..."
-ssh-add ~/.ssh/id_ecdsa
+ssh-add /home/vagrant/.ssh/id_ecdsa
 
 # Copy the public key to server1 for authentication
 echo "Copying public key to server1 for authentication..."
-ssh-copy-id -i ~/.ssh/id_ecdsa.pub "vagrant@${SERVER1_IP}"
+ssh-copy-id -i /home/vagrant/.ssh/id_ecdsa.pub "vagrant@${SERVER1_IP}"
 
 # Debug: Display the public key for troubleshooting
 echo "Contents of public key:"
-cat ~/.ssh/id_ecdsa.pub
+cat /home/vagrant/.ssh/id_ecdsa.pub
 
-# Copy the file from server2 to server1
-echo "Copying file from server2 to server1..."
-
-scp "${SERVER2_IP}:${SOURCE_PATH}" "${SERVER1_IP}:${DEST_PATH}"
+# Copy the file from server1 to server2
+echo "Copying file from server1 to server2..."
+scp "${SERVER1_IP}:${SOURCE_PATH}" "${SERVER2_IP}:${DEST_PATH}"
 
 echo "File copied successfully!"
