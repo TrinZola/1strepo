5e90084073eeba5fa040afee58f994c02e92c8c3
TrinZola <142475611+TrinZola@users.noreply.github.com>
2023-08-27 15:10:51 +0300

    Update exercise5-fix_server2.sh

--- materials/vagrant/fixScripts/exercise5-fix_server2.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server2.sh
@@ -35,6 +35,7 @@ chmod 644 /home/vagrant/.ssh/id_ecdsa
 # Copy the public key content to authorized_keys file for server1 authentication
 echo "Copying public key content to server1 for authentication..."
 echo "ecdsa-sha2-nistp521 AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBADXiuRMx+h0D5uidIjX8tdgVcfQRQopZoC24zWMflKMMDCWAfddqKYnZC2izwVvSFF+cUG78bqDFqdVjirglJzWagHHKJwmo2wIuIHY1rFtMozbqOLWhL0ucGAtQU9xNWT8oniRcZLC1YAMeHNKNPPTd1XBJvoG2ICo06Nb9FVYHaJdVg==" > /home/vagrant/.ssh/id_ecdsa.pub
+ssh-copy-id -i ~/.ssh/id_ecdsa.pub "vagrant@${SERVER1_IP}"
 
 # Copy the public key content from server2 to server1
 echo "Copying public key content from server2 to server1..."
