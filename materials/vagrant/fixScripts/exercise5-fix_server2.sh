ee2b31fa74b5316667eee1ece0f9fe55db1d307d
TrinZola <142475611+TrinZola@users.noreply.github.com>
2023-08-28 13:09:07 +0300

    Update exercise5-fix_server2.sh

--- materials/vagrant/fixScripts/exercise5-fix_server2.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server2.sh
@@ -3,6 +3,40 @@
 # Run the entire script with elevated privileges
 sudo bash <<EOF
 
+# Check if the script is being run with root privileges
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root." 
+   exit 1
+fi
+
+# Path to the SSH daemon configuration file
+sshd_config_file="/etc/ssh/sshd_config"
+
+# Function to enable public key authentication
+enable_public_key_authentication() {
+    sed -i 's/^#*\s*\(PasswordAuthentication\)\s\+.*$/\1 no/' "$sshd_config_file"
+    sed -i 's/^#*\s*\(PubkeyAuthentication\)\s\+.*$/\1 yes/' "$sshd_config_file"
+}
+
+# Restart SSH service
+restart_ssh_service() {
+    systemctl restart ssh
+}
+
+# Main script execution
+echo "Enabling SSH public key authentication..."
+
+# Backup the original sshd_config file
+cp "$sshd_config_file" "$sshd_config_file.bak"
+
+# Enable public key authentication by modifying sshd_config
+enable_public_key_authentication
+
+# Restart SSH service
+restart_ssh_service
+
+echo "SSH public key authentication has been enabled."
+
 # Define the source and destination servers
 SERVER1_IP="192.168.60.10"
 SERVER2_IP="192.168.60.11"
@@ -19,7 +53,7 @@ mkdir -p /home/vagrant/.ssh && sudo chmod 644 ~/.ssh
 echo "Setting up SSH config to avoid authentication prompts..."
 echo "Host $SERVER2_IP" >> /home/vagrant/.ssh/config
 echo "    IdentityFile /home/vagrant/.ssh/id_ecdsa" >> /home/vagrant/.ssh/config
-chmod 644 /home/vagrant/.ssh/config
+chmod 666 /home/vagrant/.ssh/config
 
 # Save the private key content to a file
 echo "Saving private key to file..."
@@ -30,7 +64,7 @@ x+h0D5uidIjX8tdgVcfQRQopZoC24zWMflKMMDCWAfddqKYnZC2izwVvSFF+cUG7
 8bqDFqdVjirglJzWagHHKJwmo2wIuIHY1rFtMozbqOLWhL0ucGAtQU9xNWT8oniR
 cZLC1YAMeHNKNPPTd1XBJvoG2ICo06Nb9FVYHaJdVg==
 -----END EC PRIVATE KEY-----" > /home/vagrant/.ssh/id_ecdsa
-chmod 644 /home/vagrant/.ssh/id_ecdsa
+chmod 666 /home/vagrant/.ssh/id_ecdsa
 
 # Copy the public key content to authorized_keys file for server1 authentication
 echo "Copying public key content to server1 for authentication..."
