77f1655c17db6dd5959ee99b7c523cae7b3efce7
TrinZola <142475611+TrinZola@users.noreply.github.com>
2023-08-29 16:20:05 +0300

    Update exercise5-fix_server2.sh

--- materials/vagrant/fixScripts/exercise5-fix_server2.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server2.sh
@@ -1,67 +1,27 @@
 #!/bin/bash
 
-# Run the entire script with elevated privileges
-sudo bash <<EOF
-
-# Check if the script is being run with root privileges
-if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root." 
-   exit 1
-fi
-
-# Path to the SSH daemon configuration file
-sshd_config_file="/etc/ssh/sshd_config"
-
-# Path to the authorized_keys file
-authorized_keys_file="/home/vagrant/.ssh/authorized_keys"
-
-# Function to configure SSH client to avoid host key checking
-configure_ssh_client() {
-    echo -e "Host *\n\tStrictHostKeyChecking no\n\tUserKnownHostsFile=/dev/null" >> "$ssh_config_file"
-}
-
-# Function to enable public key authentication
-enable_public_key_authentication() {
-    sed -i 's/^#*\s*\(PasswordAuthentication\)\s\+.*$/\1 no/' "$sshd_config_file"
-    sed -i 's/^#*\s*\(PubkeyAuthentication\)\s\+.*$/\1 yes/' "$sshd_config_file"
-    sed -i 's/^#*\s*\(AuthorizedKeysFile\)\s\+.*$/\1 /home/vagrant/.ssh/authorized_keys' "$sshd_config_file"
-}
-
-# Restart SSH service
-restart_ssh_service() {
-    systemctl restart ssh
-}
-
-# Define the source and destination servers
+# Update with the appropriate values
+SSH_USER="vagrant"
 SERVER1_IP="192.168.60.10"
 SERVER2_IP="192.168.60.11"
 
-# Create .ssh directory with appropriate permissions if it doesn't exist
-echo "Creating .ssh directory and setting permissions..."
-mkdir -p /home/vagrant/.ssh && sudo chmod 700 ~/.ssh
+# Store the public key in a variable
+PUBLIC_KEY="-----BEGIN PUBLIC KEY-----
+MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCD+is0cnPCSaA2QsC6U20Ds3Dm
+9lvxYO5WoPo1TtFfKhl+H2P7z3/zdTHSqBRndficz2jpQWccxlHDHQUr04wpO4Hh
+7uH+ndahGvz0ELWNbLINm7+iyezH2Zs3FlCH7Sedg2mmhjGbt0kP5USoJEbgRHwX
+OeVlPiw6WlWRbaBHZQIDAQAB
+-----END PUBLIC KEY-----"
 
-# Set up SSH config to avoid authentication prompts
-echo "Setting up SSH config to avoid authentication prompts..."
-echo "Host $SERVER2_IP" >> /home/vagrant/.ssh/config
-echo "    IdentityFile /home/vagrant/.ssh/id_ecdsa" >> /home/vagrant/.ssh/config
-chmod 644 /home/vagrant/.ssh/config
-# Generate an SSH key pair (ECDSA)
-echo "Generating SSH key pair..."
-ssh-keygen -t ecdsa -f /home/vagrant/.ssh/id_ecdsa -N ""
+# Create a function to install the public key
+install_public_key() {
+    local SERVER_IP="$1"
+    ssh "$SSH_USER@$SERVER_IP" "mkdir -p ~/.ssh && chmod 700 ~/.ssh && \
+        echo '$PUBLIC_KEY' >> ~/.ssh/authorized_keys && \
+        chmod 600 ~/.ssh/authorized_keys"
+    echo "SSH key has been installed on $SSH_USER@$SERVER_IP."
+}
 
-chmod 644 /home/vagrant/.ssh/id_ecdsa
-
-# Add the private key to the SSH agent
-echo "Adding private key to SSH agent..."
-ssh-add /home/vagrant/.ssh/id_ecdsa
-
-cat /home/vagrant/.ssh/id_ecdsa.pub >> "$authorized_keys_file"
-ssh-copy-id -i /home/vagrant/.ssh/id_ecdsa.pub "vagrant@${SERVER1_IP}"
-
-# Copy the public key content from server2 to server1
-echo "Copying public key content from server2 to server1..."
-scp -F "/home/vagrant/.ssh/config" /home/vagrant/.ssh/id_ecdsa.pub "vagrant@192.168.60.10:/home/vagrant/.ssh/authorized_keys"
-
-echo "Public key content copied successfully!"
-
-EOF
+# Install the public key on both servers
+install_public_key "$SERVER1_IP"
+install_public_key "$SERVER2_IP"
