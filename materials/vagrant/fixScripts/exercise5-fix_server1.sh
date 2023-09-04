328073577afb4d92696b623f4de4f44fbd9ef6c9
TrinZola <142475611+TrinZola@users.noreply.github.com>
2023-09-04 16:34:40 +0300

    Update exercise5-fix_server1.sh

--- materials/vagrant/fixScripts/exercise5-fix_server1.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,38 +1,16 @@
 #!/bin/bash
 
-# Run the entire script with elevated privileges
-sudo bash <<EOF
+sudo apt-get install sshpass -y
 
-# Define the source and destination servers
-SERVER1_IP="192.168.60.10"
-SERVER2_IP="192.168.60.11"
+ssh-keygen -t rsa -N '' -f /home/vagrant/.ssh/id_rsa <<< y
+chown vagrant:vagrant /home/vagrant/.ssh/id_rsa
+chown vagrant:vagrant /home/vagrant/.ssh/id_rsa.pub
 
-# Define the source and destination paths
-SOURCE_PATH="/home/vagrant/.ssh/authorized_keys"
-DEST_PATH="~/"
+chmod 764 /home/vagrant/.ssh/id_rsa.pub
 
-# Create .ssh directory with appropriate permissions if it doesn't exist
-echo "Creating .ssh directory and setting permissions..."
-mkdir -p ~/.ssh && sudo chmod 700 ~/.ssh
+echo 'sshpass -p vagrant ssh-copy-id -o "StrictHostKeyChecking=no" -i /home/vagrant/.ssh/id_rsa.pub vagrant@192.168.60.11' > /home/vagrant/sshing.sh
 
-# Generate an SSH key pair (ECDSA)
-echo "Generating SSH key pair..."
-ssh-keygen -t ecdsa -f ~/.ssh/id_ecdsa -N ""
+chown vagrant:vagrant /home/vagrant/sshing.sh
+chmod 774 /home/vagrant/sshing.sh
 
-# Add the private key to the SSH agent
-echo "Adding private key to SSH agent..."
-ssh-add ~/.ssh/id_ecdsa
-
-# Copy the public key to server1 for authentication
-echo "Copying public key to server1 for authentication..."
-ssh-copy-id -i ~/.ssh/id_ecdsa.pub "vagrant@${SERVER1_IP}"
-
-# Debug: Display the public key for troubleshooting
-echo "Contents of public key:"
-cat ~/.ssh/id_ecdsa.pub
-
-# Copy the file from server1 to server2
-echo "Copying file from server1 to server2..."
-scp "${SERVER1_IP}:${SOURCE_PATH}" "${SERVER2_IP}:${DEST_PATH}"
-
-echo "File copied successfully!"
+sudo sed -i '$a\    StrictHostKeyChecking no' /etc/ssh/ssh_config
