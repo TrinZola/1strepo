8915e55cd188986a14c76b10da404dabe93285c6
TrinZola <142475611+TrinZola@users.noreply.github.com>
2023-09-04 16:27:16 +0300

    Update exercise5-fix_server2.sh

--- materials/vagrant/fixScripts/exercise5-fix_server2.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server2.sh
@@ -24,7 +24,6 @@ chown vagrant:vagrant /home/vagrant/.ssh/id_rsa
 chown vagrant:vagrant /home/vagrant/.ssh/id_rsa.pub
 
 echo "Changing permisions"
-#chmod 764 /home/vagrant/.ssh/id_rsa
 chmod 764 /home/vagrant/.ssh/id_rsa.pub
 
 echo "starting sshpass ssh-copy-id to server1(192.168.60.10)"
@@ -33,22 +32,4 @@ sshpass -p vagrant ssh-copy-id -o "StrictHostKeyChecking=no"  -i  /home/vagrant/
 
 echo "+++++++++>>>>>Exacuting script sshing.sh in server1(192.168.60.10)"
 ssh -vvv -tt -o "StrictHostKeyChecking=no" -i /home/vagrant/.ssh/id_rsa vagrant@192.168.60.10 '/usr/bin/bash /home/vagrant/sshing.sh'
-#ssh -vvv -tt -o "StrictHostKeyChecking=no" -i /home/vagrant/.ssh/id_rsa vagrant@192.168.60.10 '/usr/bin/bash /home/vagrant/sshing1.sh'
 sudo sed -i '$a\    StrictHostKeyChecking no' /etc/ssh/ssh_config
-
-
-
-#sshpass -p vagrant ssh-copy-id -o "StrictHostKeyChecking=no"  -i \
-  #~/.ssh/id_rsa.pub 192.168.60.10
-
-#sudo service ssh restart
-
-#sudo ssh -o "StrictHostKeyChecking=no" 192.168.60.10  \
-  #'sshpass -p vagrant ssh-copy-id -o "StrictHostKeyChecking=no"  \
-  #-i ~/.ssh/id_rsa.pub server2'
-#sudo ssh -o "StrictHostKeyChecking=no" 192.168.60.10 'sudo service ssh restart'
-
-#echo "\n>>> running commands"
-#cat ~/.ssh/id_rsa.pub | ssh -t 192.168.60.10 "mkdir -p ~/.ssh && chmod 700 ~/.ssh && cat >> ~/.ssh/authorized_keys && chmod 600 ~/.ssh/authorized_keys"
-
-#sudo service ssh restart
