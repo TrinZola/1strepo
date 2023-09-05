fc98a5e785b69a57e1d4e7afc6966ab4a40154ae
TrinZola <142475611+TrinZola@users.noreply.github.com>
2023-09-05 11:31:56 +0300

    Update exercise5-fix_server2.sh

--- materials/vagrant/fixScripts/exercise5-fix_server2.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server2.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+sudo sed -i '$a\192.168.60.10	 server1 server1' /etc/hosts
+
+# changing PasswordAuthentication value to 'yes'
+sudo sed -i "s:PasswordAuthentication no:PasswordAuthentication yes :g" \
+  /etc/ssh/sshd_config
+
+# restart service so changes will take effect
+sudo service sshd restart
+echo "Updating System"
+sudo apt-get update && sudo apt-get upgrade
+
 echo "Installing sshpass"
 sudo apt-get install sshpass -y
 echo "Generating keys"
