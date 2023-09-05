e316cec58776a7be18ce3c50272f4a284da24f40
TrinZola <142475611+TrinZola@users.noreply.github.com>
2023-09-05 12:22:52 +0300

    Update exercise3-fix.sh

--- materials/vagrant/fixScripts/exercise3-fix.sh
+++ materials/vagrant/fixScripts/exercise3-fix.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#add fix to exercise3 here
+
 #replace string in '/etc/apache2/sites-available/000-default.conf '
 sudo sed -i 's/Require all denied/Require all granted/'    \
   /etc/apache2/sites-available/000-default.conf
